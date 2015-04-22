dacb793987ef7ca15284cd57350b3c4ebf219e10
MarinaUrazova <marishka.ura@mail.ru>
2015-04-22 10:41:23 +0400

    Finished

--- OrderedSet.h
+++ OrderedSet.h
@@ -25,39 +25,55 @@ public:
 		int n = 0;
 
 		for (int i = 0; i < size_data; i++) {
-            if (data[i] != NULL)
-                while (cmp_func(new_element, data[i]) < 1)
-                    n = i + 1;
-            else n = i;
+            if ((data[i] != NULL) && (data[i+1] != NULL)) {
+                if ((cmp_func(new_element, data[i]) == 0) && (cmp_func(new_element, data[i+1]) == 1)) n = i + 1;
+            }
         }
 
-		for (int j = size_data - 1; j > n; j--) {
+		for (int j = size_data - 1; j >= n; j--) {
             data[j+1] = data[j];
         }
 		data[n] = new_element;
 
+		size_data++;
     }
 
     int remove( char* element ) {
-        int k=0;
+        int k = 0;
+
 		for (int i = 0; i < size_data; i++) {
-            if(data[i] == element) k = i;
+            if(data[i] == element) { k = i;
+                break;
+            }
             else k = -1;
         }
-        if (k == -1) cout << 'Error';
-        else    for (int j = k; j < size_data; j++)
-                    data[k] = data[k+1];
+
+        if (k == -1) cout << "Невозможно удалить элемент " << element << endl;
+        else { cout << "Элемент " << element << " успешно удалён" << endl;
+
+        for (int j = k; j < size_data - 1; j++)
+            data[j] = data[j+1];
+        }
+        size_data--;
     }
 
     int search( char* element ) {
-        int k=0;
-		for (int i = 0; i < size_data; i++) {
-            if(data[i] == element) {
-                k = i;
-                cout << k;
-            }
-            else cout << 'Error';
-        }
-	}
+        int k = 0;
 
+		for (int i = 0; i < size_data; i++) {
+            if(data[i] == element) { k = i;
+                cout << "Номер элемента " << element << " : " << k << endl;
+                break;
+            }
+            else k = -1;
+        }
+
+        if (k == -1) cout << "Элемент " << element << " не найден" << endl;
+    }
+
+	void print() {
+        for (int i = 0; i < size_data; i++) {
+            if (data[i] != NULL)
+                cout << data[i] << endl; }
+    }
 };
--- test.cpp
+++ test.cpp
@@ -3,13 +3,13 @@
 #include "OrderedSet.h"
 using namespace std;
 
-int cmp(char* a1, char* a2) {
+int cmp_func(char* a1, char* a2) {
     if (strlen(a1) <= strlen(a2)) return 0;
     else return 1;
 }
 
 char* makestring() {
-    int n = rand()%10;
+    int n = rand()%10 + 1;
     char array[26] = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'};
     char* str = (char*)calloc(sizeof(char), n);
     for(int i=0; i < n; i++)
@@ -19,19 +19,33 @@ char* makestring() {
 
 int main(int argc, const char* argv[]) {
     srand(time(NULL));
-
     int size_data;
     cout << 'Enter number of elements in ordered set.' << endl;
     cin >> size_data;
 
-    OrderedSet *MyOrderedSet = new OrderedSet(size_data, &cmp);
+    OrderedSet *MyOrderedSet = new OrderedSet(size_data, &cmp_func);
 
-    for (int i=0; i<size_data; i++) {
+    /*for (int i=0; i<size_data; i++) {
         char* tmp;
         tmp = makestring();
-        cout << tmp << endl;
         MyOrderedSet -> add(tmp);
     }
+        MyOrderedSet -> print();*/
 
-    /*MyOrderedSet -> add("str");*/
+    MyOrderedSet -> add("str");
+    MyOrderedSet -> add("strrr");
+    MyOrderedSet -> add("strrrrr");
+    MyOrderedSet -> add("strr");
+    MyOrderedSet -> add("strrrrrr");
+    MyOrderedSet -> add("strrrr");
+    MyOrderedSet -> print();
+
+    MyOrderedSet -> remove("st");
+    MyOrderedSet -> print();
+
+    MyOrderedSet -> remove("strr");
+    MyOrderedSet -> print();
+
+    MyOrderedSet -> search("st");
+    MyOrderedSet -> search("strr");
 }
